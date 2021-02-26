89d469264c8166c87467b9c0634f0c076cc1846a
Alexander Miesen <alexmiesen@hotmail.com>
2021-02-27 00:11:09 +1100

    users can now sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
 	def index
 		@users = User.all
 	end
@@ -11,7 +10,7 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
-	
+
 	def edit
 		@user = User.find(params[:id])
 	end
@@ -19,17 +18,18 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to @user, notice: "Thanks for signing up!"
 		else
 			render :new
-		end 
+		end
 	end
 
 	def update
 		@user = User.find(params[:id])
 		@user.update(user_params)
 		if @user.save
-			redirect_to @user, notice: "Details updated" 
+			redirect_to @user, notice: "Details updated"
 		else
 			render :edit
 		end
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
 	def destroy
 		@user = User.find(params[:id])
 		@user.destroy
+		session[:user_id] = nil
 		redirect_to root_path, alert: "Account successfully deleted!"
 	end
 
@@ -45,7 +46,7 @@ private
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
-	end 
+	end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,15 @@ class User < ApplicationRecord
 
 	validates :password, length: { minimum: 10, allow_blank: true }
 
-	validates :username, presence: true, format: /\A[A-Z0-9]+\z/i, uniqueness: { case_sensitive: false}
+	validates :username, presence: false, format: /\A[A-Z0-9]+\z/i, uniqueness: { case_sensitive: false}
 	
 	def gravatar_id
 		Digest::MD5::hexdigest(email.downcase)
 	end
+
+	def self.authenticate(email, password)
+		user = User.find_by(email: email)
+		user && user.authenticate(password)
+	end 
+
 end
