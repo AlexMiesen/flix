675f36a423ed3ee9cf9857f17b695573c0702400
Alexander Miesen <alexmiesen@hotmail.com>
2021-03-21 23:49:15 +1100

    added admins to spec files, added an admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210215095450) do
+ActiveRecord::Schema.define(version: 20210321124713) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20210215095450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
 end
