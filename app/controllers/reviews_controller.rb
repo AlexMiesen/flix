1ebc298ecfc2aec3d54794b7c52eb8828d6129ff
Alexander Miesen <alexmiesen@hotmail.com>
2021-04-23 22:44:18 +1000

    used association to create @reviews = @user.reviews in the users controller, displayed the review's in the profile page of a user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,7 @@ class ReviewsController < ApplicationController
 
 	def create
 		@review = @movie.reviews.new(review_params)
+		@review.user = current_user
 		if @review.save 
 		redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
 		else
@@ -28,11 +29,11 @@ class ReviewsController < ApplicationController
 private
 
 	def review_params
-		params.require(:review).permit(:name, :stars, :comment)
+		params.require(:review).permit(:stars, :comment)
 	end
 	
 	def set_movie
-	 @movie = Movie.find(params[:movie_id])
+		@movie = Movie.find(params[:movie_id])
 	end
 
 
