1b01c588517e48633b6c2861d795e0af39065737
Alexander Miesen <alexmiesen@hotmail.com>
2021-05-02 18:24:53 +1000

    added a favourites model, and created the through associations named 'favourite_movies' and 'fans' for users and movies respectively

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210329062245) do
+ActiveRecord::Schema.define(version: 20210502080801) do
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_favourites_on_movie_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
