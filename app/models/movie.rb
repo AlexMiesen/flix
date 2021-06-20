19d025b2c957d4badbb39d7005ac59bb9e97e600
AlexMiesen <alex@i-motor.com.au>
2021-06-20 21:45:15 +1000

    converted flops, hits and released in scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -23,18 +23,9 @@ class Movie < ApplicationRecord
 
 	validates :rating, inclusion: { in: RATINGS }
 
-
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
-  end
-
-  def self.hits
-    where('total_gross >= 300000000').order(total_gross: :desc)
-  end
-
-  def self.flops
-    where('total_gross < 50000000').order(total_gross: :asc)
-  end
+	scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
+	scope :hits, -> { where('total_gross >= 300000000').order(total_gross: :desc) }
+	scope :flops, -> { where('total_gross < 50000000').order(total_gross: :asc) }
 
   def self.recently_added
     order('created_at desc').limit(3)
